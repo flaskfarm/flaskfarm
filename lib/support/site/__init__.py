2e46777d122363b6b54c2ee1deabd3b80421a4cf
flaskfarm <flaskfarm@gmail.com>
2022-10-21 05:02:22 +0900

    update

---
+++ lib/support/site/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+from support import SupportSC
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'tving.py')):
+    from .tving import SupportTving
+    from .wavve import SupportWavve
+else:
+    SupportTving = SupportSC.load_module_f(__file__, 'tving').SupportTving
+    SupportWavve = SupportSC.load_module_f(__file__, 'wavve').SupportWavve
