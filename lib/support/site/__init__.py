16d8f922390bc8e02f39be79a8d5f5b0aff8e9e1
flaskfarm <flaskfarm@gmail.com>
2022-11-04 11:55:15 +0900

    update

--- lib/support/site/__init__.py
+++ lib/support/site/__init__.py
@@ -4,6 +4,7 @@ from support import SupportSC
 
 try:
     if os.path.exists(os.path.join(os.path.dirname(__file__), 'tving.py')):
+        #from .cppl import SupportCppl
         from .kakaotv import SupportKakaotv
         from .seezn import SupportSeezn
         from .tving import SupportTving
@@ -13,5 +14,6 @@ try:
         SupportWavve = SupportSC.load_module_f(__file__, 'wavve').SupportWavve
         SupportSeezn = SupportSC.load_module_f(__file__, 'seezn').SupportSeezn
         SupportKakaotv = SupportSC.load_module_f(__file__, 'kakaotv').SupportKakaotv
+        #SupportCppl = SupportSC.load_module_f(__file__, 'cppl').SupportCppl
 except:
     pass
