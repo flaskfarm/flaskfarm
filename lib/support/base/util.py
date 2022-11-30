f6b5a39129fe7bd6e328ccb22a93b675cfb1de2b
flaskfarm <flaskfarm@gmail.com>
2022-12-01 00:48:01 +0900

    update

--- lib/support/base/util.py
+++ lib/support/base/util.py
@@ -29,9 +29,9 @@ class SupportUtil(object):
     def sizeof_fmt(cls, num, suffix='Bytes'):
         for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
             if abs(num) < 1024.0:
-                return "%3.1f%s%s" % (num, unit, suffix)
+                return "%3.1f %s%s" % (num, unit, suffix)
             num /= 1024.0
-        return "%.1f%s%s" % (num, 'Y', suffix)
+        return "%.1f %s%s" % (num, 'Y', suffix)
 
     @classmethod
     def is_arm(cls):
