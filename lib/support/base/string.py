f651f5ffb2a5ce4484584c45a7379b99faf7ec14
soju6jan <cybersol@naver.com>
2024-06-01 08:42:18 +0900

    4.1.3 : SuppportString.remove_emoji 추가

--- lib/support/base/string.py
+++ lib/support/base/string.py
@@ -61,10 +61,25 @@ class SupportString(object):
     def remove_emoji(cls, text, char=''):
         import re
         emoji_pattern = re.compile("["
-                u"\U0001F600-\U0001F64F"  # emoticons
-                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002500-\U00002BEF"  # chinese char
+                                u"\U00002702-\U000027B0"
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                u"\U0001f926-\U0001f937"
+                                u"\U00010000-\U0010ffff"
+                                u"\u2640-\u2642"
+                                u"\u2600-\u2B55"
+                                u"\u200d"
+                                u"\u23cf"
+                                u"\u23e9"
+                                u"\u231a"
+                                u"\ufe0f"  # dingbats
+                                u"\u3030"
                                 "]+", flags=re.UNICODE)
-        return emoji_pattern.sub(char, text) # no emoji
-        #print(emoji_pattern.findall(text))  # only emoji
+        # Remove emojis from the text
+        text = emoji_pattern.sub(char, text)
+        return text
